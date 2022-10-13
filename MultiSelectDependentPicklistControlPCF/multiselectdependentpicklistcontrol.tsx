130b35ea8336c19a8cdc0473a9459a0d8f77c04a
whoppacfm <13801775+whoppacfm@users.noreply.github.com>
2022-10-13 14:14:16 +0200

    Update multiselectdependentpicklistcontrol.tsx

--- MultiSelectDependentPicklistControlPCF/multiselectdependentpicklistcontrol.tsx
+++ MultiSelectDependentPicklistControlPCF/multiselectdependentpicklistcontrol.tsx
@@ -29,6 +29,7 @@ import {
     mergeStyleSets,
     HoverCard,
     IExpandingCardProps,
+    Selection,
     ThemeProvider,
   } from '@fluentui/react';
   import { createListItems, IExampleItem } from '@fluentui/example-data';
@@ -44,23 +45,9 @@ if(href.indexOf("127.") > -1 || href.indexOf("localhost") > -1) {
 }
 var CRM_TEST_MODE = 0;
 
-//-------------------------
-//Data Definitions
-//-------------------------
-class CCustomData {
-    customdata1:string;
-    customdata2:string;
-    constructor(customdata1?:string, customdata2?:string) {
-        if(customdata1) {
-            this.customdata1 = customdata1;
-        }
-        if(customdata2) {
-            this.customdata2 = customdata2;
-        }
-    };
-}
 
 //UI Stuff
+/*
 const options: IComboBoxOption[] = [
     { key: 'Header1', text: 'First heading', itemType: SelectableOptionMenuItemType.Header },
     { key: 'A', text: 'Option A' },
@@ -79,42 +66,15 @@ const options: IComboBoxOption[] = [
   // Optional styling to make the example look nicer
   const comboBoxStyles: Partial<IComboBoxStyles> = { root: { maxWidth: 300 } };
   const buttonStyles: Partial<IButtonStyles> = { root: { display: 'block', margin: '10px 0 20px' } };
-
-
-
-
-//-------------------------
-//Redux
-//-------------------------
-function reducerCustomData(customdata:CCustomData[] = new Array<CCustomData>(), action:AnyAction) {
-    switch (action.type) {
-        case "CUSTOMDATA/SETCUSTOMDATA":
-            return {
-                customdata: action.data1
-            }
-        default:
-        return customdata;
-    }
-}
-
-//const reduxStore = createStore(reducerCustomData as any);
-const reduxStore = configureStore({ reducer: reducerCustomData });
-
-//Save data to store
-//dispatch({ type: "CUSTOMDATA/SETCUSTOMDATA", data1: data });
-
-//Get data from store
-//let customdata = useSelector((customdata: CCustomData[]) => customdata);
-//let cdata1 = customdata.customdata1;
-
+*/
 
 function MultiSelectDependentPicklistControl(props:any) {
 
     //-------------------------
     //UI Stuff
     //-------------------------
-    const comboBoxRef = React.useRef<IComboBox>(null);
-    const onOpenClick = React.useCallback(() => comboBoxRef.current?.focus(true), []);
+    //const comboBoxRef = React.useRef<IComboBox>(null);
+    //const onOpenClick = React.useCallback(() => comboBoxRef.current?.focus(true), []);
 
     const classNames = mergeStyleSets({
         compactCard: {
@@ -136,141 +96,173 @@ function MultiSelectDependentPicklistControl(props:any) {
         },
       });
       
-      interface IExampleItem {
+      interface IPicklistItem {
         label:string;
         value:string;
       }
 
-      const items: IExampleItem[] = [{"label":"123", "value":"fsdfdsf"},{"label":"543", "value":"fdss"},{"label":"3456", "value":"xcvx"}]; //createListItems(10);
-      
-      const buildColumn = (): IColumn[] => {
-        return buildColumns(items).filter(column => column.name === 'label');
-      };
-      
-      const onRenderCompactCard = (item: IExampleItem): JSX.Element => {
-        return (
-          <div className={classNames.compactCard}>
-            <a target="_blank" href={`http://wikipedia.org/wiki/${item.label}`}>
-              {item.label}
-            </a>
-          </div>
-        );
-      };
-      
-      const columns: IColumn[] = buildColumn();
-      
-      const onRenderExpandedCard = (item: IExampleItem): JSX.Element => {
-        return (
-          <div className={classNames.expandedCard}>
-            {item.label}
-            <DetailsList setKey="expandedCardSet" items={items} columns={columns} />
-          </div>
-        );
-      };
-      
-      const onRenderItemColumn = (item: IExampleItem, index: number, column: IColumn): JSX.Element | React.ReactText => {
-        const expandingCardProps: IExpandingCardProps = {
-          onRenderCompactCard: onRenderCompactCard,
-          onRenderExpandedCard: onRenderExpandedCard,
-          renderData: item,
-        };
-        if (column.key === 'label') {
-          return (
-            <HoverCard expandingCardProps={expandingCardProps} instantOpenOnClick={true}>
-              <div className={classNames.item}>{item.label}</div>
-            </HoverCard>
-          );
-        }
-        return item[column.key as keyof IExampleItem];
-      };
-
     //-------------------------
     //State
     //-------------------------
-    /*
-    const [customState, setCustomState] = React.useState({ 
-        statevar1: "",
-        statevar2: ""
+    const [picklist2items, setPicklist2items] = React.useState({ 
+       data : [] as Array<IPicklistItem>,
     });
-    */
+    const [picklist2itemsFiltered, setPicklist2itemsFiltered] = React.useState({ 
+      data : [] as Array<IPicklistItem>,
+   });
 
-    //-------------------------
-    //Init
-    //-------------------------
-
+    //-----------------------
     //Get current record data
-    let currentFntityId = (props.context.mode as any).contextInfo.entityId;
+    //-----------------------
+    let currentEntityId = (props.context.mode as any).contextInfo.entityId;
     let currentEntityTypeName = (props.context.mode as any).contextInfo.entityTypeName;
     let currentEntityRecordName = (props.context.mode as any).contextInfo.entityRecordName;
 
-    //Get current control field values
 
-    //Lookup Field Example
-    //let lookupfield_currentValue = props.context.parameters.BoundLookupField.raw[0];
-    //let lookupfield_currentId = lookupfield_currentValue.id;
-    //let lookupfield_currentEntityType = lookupfield_currentValue.entityType;
-    //let lookupfield_currentRecordName = lookupfield_currentValue.name;
-
-    //Datetime Example
-    //let dateval = context.parameters.date_input_property.raw?.toDateString();
-
+    //--------------
     //Get PCF Config
-    /*
-    let config_fields:Array<string> = [];
-    let config_lists:string = "";
-
-    if(props.context.parameters.Fields.raw!=null) {
-        config_fields = props.context.parameters?.Fields?.raw.split(",");
+    //--------------
+    interface IMapping {
+      field1value:number;
+      field2values:Array<number>;
+    }
+    interface IMappingConfig {
+      field1name:string;
+      field2name:string;
+      mappings:Array<IMapping>;
+    }
+    let mapping_config:IMappingConfig;
+    if(DATA_SOURCE=="TEST") {
+      mapping_config = JSON.parse('{"field1name":"sup_categorycode", "field2name":"sup_subcategorycode", "mappings":[{"field1value":43534534, "field2values":[346433,43535]}]}');
+    }
+    else {
+      mapping_config = JSON.parse(props.context.parameters?.mapping_config_property?.raw);
     }
 
-    if(props.context.parameters?.Lists?.raw!=null) {
-        config_lists = props.context.parameters?.Lists?.raw;
-    }
-    */
-    
-    //Init Dispatch
-    const dispatch = useDispatch();
-    
+
+    //---------------------
     //Init data / load data
+    //---------------------
+    const itemsPicklist1: IPicklistItem[] = [{"label":"ABT543564", "value":"43534534"},{"label":"ABT23334", "value":"434534"},{"label":"ABT33423435", "value":"4534534"},{"label":"ABT334233435", "value":"453234534"},{"label":"ABT334523435", "value":"45345324"}];
+    const columnsPicklist1: IColumn[] =  [ { key: 'Category', name: 'Category', fieldName: 'label', minWidth: 100, maxWidth: 200, isResizable: true },];
+    const columnsPicklist2: IColumn[] =  [ { key: 'Subcategory', name: 'Subcategory', fieldName: 'label', minWidth: 100, maxWidth: 200, isResizable: true },];
+
     useEffect(() => {
-
         if(DATA_SOURCE=="TEST") {
-            
             //Init test data
-            /*
-            setCustomState({"statevar1": "test123", "statevar2":"test123"});
-
-            const testData:CCustomData[] = [
-                {"customdata1":"1","customdata2":"test1"},
-                {"customdata1":"2","customdata2":"test2"},
-                {"customdata1":"3","customdata2":"test3"}
-            ]
-            
-            dispatch({ type: "CUSTOMDATA/SETCUSTOMDATA", data1: testData });
-            */
+            let picklistData:Array<IPicklistItem> = [{"label":"TTZ34223", "value":"346433"},{"label":"TTZ435342", "value":"43535"},{"label":"TTZ234522", "value":"34534124"},{"label":"TTZ2342522", "value":"345334124"},{"label":"TTZ2345522", "value":"345374124"}];
+            setPicklist2items({"data":picklistData});
+            setPicklist2itemsFiltered({"data":picklistData});
         }
         else {
-
             //Load data from crm
-
         }
-
     }, []);
 
-    //Get data from store
-    let storeData1 = useSelector((customdata: any) => customdata);
-    let showStoredata:any;
-    if(storeData1!=null && storeData1.customdata!=null && storeData1.customdata.length>0) {
-        showStoredata = (storeData1.customdata as Array<CCustomData>).map((item:CCustomData) => {
-            return (
-                <>
-                    <div>{item.customdata1}</div><div>{item.customdata2}</div>
-                    <br/><br/>
-                </>
-            );
-        });
-    }
+
+    //---------
+    //Functions
+    //---------
+
+    const onRenderCompactCard = (item: IPicklistItem): JSX.Element => {
+      return (
+        <div className={classNames.compactCard}>
+          <a target="_blank" href={`http://wikipedia.org/wiki/${item.label}`}>
+            {item.label}
+          </a>
+        </div>
+      );
+    };
+
+    const onRenderExpandedCardPicklist1 = (item: IPicklistItem): JSX.Element => {
+      return (
+        <div className={classNames.expandedCard}>
+          {item.label}
+          <DetailsList setKey="expandedCardSet" items={itemsPicklist1} columns={columnsPicklist1} />
+        </div>
+      );
+    };
+
+    const onRenderExpandedCardPicklist2 = (item: IPicklistItem): JSX.Element => {
+      return (
+        <div className={classNames.expandedCard}>
+          {item.label}
+          <DetailsList setKey="expandedCardSet" items={itemsPicklist1} columns={columnsPicklist2} />
+        </div>
+      );
+    };      
     
+    const onRenderItemColumnPicklist1 = (item: IPicklistItem, index: number, column: IColumn): JSX.Element | React.ReactText => {
+      const expandingCardProps: IExpandingCardProps = {
+        onRenderCompactCard: onRenderCompactCard,
+        onRenderExpandedCard: onRenderExpandedCardPicklist1,
+        renderData: item,
+      };
+      if (column.key === 'Category') {
+        return (
+          <HoverCard expandingCardProps={expandingCardProps} instantOpenOnClick={true}>
+            <div className={classNames.item}>{item.label}</div>
+          </HoverCard>
+        );
+      }
+      return item[column.key as keyof any];
+    };
+
+    const onRenderItemColumnPicklist2 = (item: IPicklistItem, index: number, column: IColumn): JSX.Element | React.ReactText => {
+      const expandingCardProps: IExpandingCardProps = {
+        onRenderCompactCard: onRenderCompactCard,
+        onRenderExpandedCard: onRenderExpandedCardPicklist2,
+        renderData: item,
+      };
+      if (column.key === 'Subcategory') {
+        return (
+          <HoverCard expandingCardProps={expandingCardProps} instantOpenOnClick={true}>
+            <div className={classNames.item}>{item.label}</div>
+          </HoverCard>
+        );
+      }
+      return item[column.key as keyof any];
+    };          
+
+    const selectionPicklist1 = new Selection({
+      onSelectionChanged: () => {
+
+        let picklist2ShowValues:Array<number> = new Array<number>();
+        selectionPicklist1.getSelection().forEach((selectedItem:any) => {
+          
+          mapping_config.mappings.forEach((mappingItem:IMapping)=>{
+            if(mappingItem.field1value==parseInt(selectedItem.value)) {
+              picklist2ShowValues = picklist2ShowValues.concat(mappingItem.field2values);
+            }
+          });
+
+          if(picklist2ShowValues.length>0) {
+            let newFilteredValues = picklist2items.data.filter((item:IPicklistItem) => {
+
+              if(picklist2ShowValues.indexOf(parseInt(item.value))>-1) {
+                return true;
+              }
+
+              return false;
+
+            });
+
+            debugger;
+
+            setPicklist2itemsFiltered({"data":newFilteredValues});
+          }
+          else {
+            setPicklist2itemsFiltered({"data":picklist2items.data});
+          }
+          
+        });
+      },
+    });
+
+
+    //-------
+    //Styles
+    //-------
     const getStylesLeftPicklist = () => {
         return {
             root: {
@@ -293,14 +285,16 @@ function MultiSelectDependentPicklistControl(props:any) {
             }
         }
     };    
-    
+
+
     return (
        <div>
         <ThemeProvider>
-           <DetailsList styles={getStylesLeftPicklist} setKey="hoverSet" items={items} columns={columns} onRenderItemColumn={onRenderItemColumn} />
-           <DetailsList styles={getStylesRightPicklist} setKey="hoverSet" items={items} columns={columns} onRenderItemColumn={onRenderItemColumn} />
+           <DetailsList styles={getStylesLeftPicklist} setKey="picklist1Set" items={itemsPicklist1} columns={columnsPicklist1} selection={selectionPicklist1} onRenderItemColumn={onRenderItemColumnPicklist1} />
+           <DetailsList styles={getStylesRightPicklist} setKey="picklist2Set" items={picklist2itemsFiltered.data} columns={columnsPicklist2} onRenderItemColumn={onRenderItemColumnPicklist2} />
         </ThemeProvider>
-      
+
+
         {/*
         <ComboBox
           componentRef={comboBoxRef}
@@ -325,16 +319,16 @@ function MultiSelectDependentPicklistControl(props:any) {
           styles={comboBoxStyles}
         />
         */}
+
+        
       </div>        
     );
 }
 
 export function Render(context:any, container:any, theobj:object) {
     ReactDOM.render(
-        <Provider store={reduxStore}>
-            <div><MultiSelectDependentPicklistControl context={context} theobj={theobj} /></div>
-        </Provider>
-        , container
-      );
+      <div><MultiSelectDependentPicklistControl context={context} theobj={theobj} /></div>
+      , container
+    );
 }
 
